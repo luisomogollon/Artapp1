8dcee86452990b6e317c6015e97765a444ac9a35
LouMogollonDev <91619676+luisomogollon@users.noreply.github.com>
2022-09-26 17:37:09 -0400

    cambio

--- src/routes/Catalogue.jsx
+++ src/routes/Catalogue.jsx
@@ -7,6 +7,19 @@ import imagenes from "../assets/imagenes";
 const Catalogue = () => {
   const [qrImage, setImage] = useState(false);
 
+ 
+const [modal, setModal] = useState(false);
+
+const toggleModal = () => {
+  setModal(!modal);
+};
+
+if(modal) {
+  document.body.classList.add('active-modal')
+} else {
+  document.body.classList.remove('active-modal')
+}
+
   const toggleQR = () => {
     setImage(!qrImage);
   };
@@ -16,11 +29,34 @@ const Catalogue = () => {
       <section className="h-auto  bg-gray-100">
         <div className=" justify-center  text-center py-11 px-10  sm:px-6 lg:px-8 sm:text-center">
           <p className="mt-1 text-4xl  italic font-bold text-gray-700 sm:text-5xl sm:tracking-tight lg:text-5xl">
-            Digital Art Catalog.
+            Digital Art Catalog
           </p>
         </div>
       </section>
 
+      <button onClick={toggleModal} class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="popup-modal">
+  Toggle modal
+</button>
+{modal && (
+<div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full">
+    <div class="relative p-4 w-full max-w-md h-full md:h-auto">
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="popup-modal">
+                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                <span class="sr-only" className="close-modal" onClick={toggleModal} >Close modal</span>
+            </button>
+            <div class="p-6 text-center">
+                <svg aria-hidden="true" class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
+                <button data-modal-toggle="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                    Yes, I'm sure
+                </button>
+                <button data-modal-toggle="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
+            </div>
+        </div>
+    </div>
+</div>
+)}
       <div className="bg-gray-100 w-100 min-h-screen gap-6  flex-wrap flex justify-center items-center">
         <div className="w-72 p-2  bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
           <img
