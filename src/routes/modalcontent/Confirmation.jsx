0c62f95d2690b83fc99a38087e9649429894dd20
LouMogollonDev <91619676+luisomogollon@users.noreply.github.com>
2022-09-27 15:57:40 -0400

    cambios finales

---
+++ src/routes/modalcontent/Confirmation.jsx
@@ -0,0 +1,35 @@
+import { AiOutlineWarning } from "react-icons/ai";
+
+const ConfirmationModalContent = () => {
+  return (
+    <>
+      <div className="flex items-c text-center justify-between p-3 border-b border-solid border-slate-200 rounded-t">
+        <h3 className="text-lg font-extrabold leading-1 italic text-gray-900   ">
+          ¿Compartir Este Cuadro?
+        </h3>
+      </div>
+      <div className="relative p-7 flex-auto">
+        <div className="p-1 text-center">
+          <svg
+            aria-hidden="true"
+            className="mx-auto mb-2 w-10 h-10 text-indigo-500 dark:text-gray-200"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 15 15"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <AiOutlineWarning />
+          </svg>
+        </div>
+
+        <p className="text-sm font-semibold leading-5 text-start  text-gray-700 mt-3 tracking-normal  dark:text-gray-600   ">
+          ¿ Estas Seguro de querer Compartir este Certificado? una vez
+          Compartido no hay manera de deshacer esta accion y el certificado sera
+          propiedad del nuevo usuario.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default ConfirmationModalContent;
---
+++ src/routes/modalcontent/Form.jsx
@@ -0,0 +1,69 @@
+import { AiOutlineUserAdd } from "react-icons/ai";
+
+const ModalForm = () => {
+  return (
+    <>
+      <div className="p-1 text-center">
+        <svg
+          aria-hidden="true"
+          className="mx-auto mb-1 mt-2 w-10 h-10 text-gray-600 "
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 15 15"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <AiOutlineUserAdd />
+        </svg>
+      </div>
+      <form>
+        <div className="mb-6 ">
+          <label
+            for="email"
+            className="block mb-2 text-start text-sm font-medium text-gray-700 "
+          >
+            User
+          </label>
+          <input
+            type="text"
+            id="text"
+            className="bg-gray-50 border border-gray-200  focus:outline-none focus:border-indigo-300 focus:ring-1 focus:ring-indigo-300 text-gray-900 text-sm rounded-sm  block w-full p-2.5 "
+            placeholder="User"
+          />
+        </div>
+        <div className="mb-6 ">
+          <label
+            for="email"
+            className="block mb-2 text-start text-sm font-medium text-gray-700 "
+          >
+            Nombre
+          </label>
+          <input
+            type="text"
+            id="text"
+            className="bg-gray-50 border border-gray-200  focus:outline-none focus:border-indigo-300 focus:ring-1 focus:ring-indigo-300 text-gray-900 text-sm rounded-sm  block w-full p-2.5 "
+            placeholder="Nombre Del Usuario"
+          />
+        </div>
+        <div className="flex items-start mb-6">
+          <div className="flex items-center h-5">
+            <input
+              id="remember"
+              type="checkbox"
+              value=""
+              className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+              required=""
+            />
+          </div>
+          <label
+            for="remember"
+            className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            Confirmar
+          </label>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default ModalForm;
